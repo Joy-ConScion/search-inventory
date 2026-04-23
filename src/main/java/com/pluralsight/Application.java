f8a255afc7bc4534798b2b885f7c9a36af89f4ad
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-23 14:18:13 -0500

    Inventory list is updated and displayed

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
+//Methods/tools
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+        System.out.println("We carry the following inventory: ");
+        for (int i = 0; i < inventory.size(); i++) {
+            Product p = inventory.get(i);
+            System.out.printf("id: %d %s - Price: $%.2f%n",
+                    p.getId(), p.getName(), p.getPrice());
+        }
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        // this method loads product objects into inventory
+        // and its details are not shown
+        Product product1 = new Product(4567, "10' 2X4 (Grade B)", 9.99f);
+        Product product2 = new Product(1234, "Hammer", 19.49f);
+        Product product3 = new Product(2345, "Box of Nails", 9.29f);
+        Product product4 = new Product(3948, "Heavy Duty Gloves", 12.29f);
+        Product product5 = new Product(1284, "Duct Tape", 8.79f);
+
+        inventory.add(product1);
+        inventory.add(product2);
+        inventory.add(product3);
+        inventory.add(product4);
+        inventory.add(product5);
+
+        return inventory;
+    }
+
+
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
 public class Product {
+    private int id;
+    private String name;
+    private float price;
+
+//    Constructor
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+// Getters
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+
+
+
 }
