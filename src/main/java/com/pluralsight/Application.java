a670d0efca20b30fed777a01b9aa7ef58f8a45a1
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-23 14:57:58 -0500

    Files added

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -9,16 +9,23 @@ public class Application {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
-        System.out.println("We carry the following inventory: ");
-        for (int i = 0; i < inventory.size(); i++) {
-            Product p = inventory.get(i);
+        ArrayList<Product> products = getProducts();
+        System.out.println("We carry the following products: ");
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
             System.out.printf("id: %d %s - Price: $%.2f%n",
                     p.getId(), p.getName(), p.getPrice());
         }
+//Data type (Class) varName : array name
+        for(Product p: products){
+            System.out.printf("id: %d %s - Price: $%.2f%n",
+                    p.getId(), p.getName(), p.getPrice());
+        }
+
+
     }
 
-    public static ArrayList<Product> getInventory() {
+    public static ArrayList<Product> getProducts() {
         ArrayList<Product> inventory = new ArrayList<Product>();
         // this method loads product objects into inventory
         // and its details are not shown
